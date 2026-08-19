52e9167710b3a84b98a41d6a5efcb0c7ff8f5ce4
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-08-19 21:11:44 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -6,31 +6,29 @@ AMI_ID="ami-0220d79f3f480ecf5" #replace with your ami id
 
 for instance in $@
 
-do 
-        INSTANCE_ID=$(aws ec2 run-instances /
-         --image-id $AMI_ID /
-         --instance-type t3.micro /
-         --security-group-ids $SG_ID / 
-         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value='$instance'}]"  /
-         --query 'Instances[0].InstanceId'  /
-         --output text)
+do
+    INSTANCE_ID=$(aws ec2 run-instances \ 
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --security-group-ids $SG_ID \ 
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId'\
+    --output text )
 
+    if [$instance == "frontend"]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text)
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text)
+    fi
 
-
-            if [ $instance == "frontend" ]; then
-                IP=$(aws ec2 describe-instances /
-                --instance-ids $INSTANCE_ID /
-                --query 'Reservations[].Instances[].PublicIpAddress' /
-                --output text
-            )
-            else
-                IP=$(aws ec2 describe-instances /
-                --instance-ids $INSTANCE_ID /
-                --query 'Reservations[].Instances[].PrivateIpAddress' /
-                --output text
-            )
-fi
-
-echo "IP Address : $IP"
+    echo "IP Address : $IP"
 
 done
