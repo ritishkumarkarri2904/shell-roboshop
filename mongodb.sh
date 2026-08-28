1d2562e4e2eaf44697d4391e34c74fd4c09057fa
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-08-28 16:36:37 +0530

    changes

---
+++ mongodb.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/logs/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e " $R Please run this script as root or with sudo privileges. $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+#create logs folder if not exists
+mkdir -p $LOGS_FOLDER
+
+VALIDATE () {
+if [ $1 -ne 0 ]; then
+    echo -e "Installing $2 is $R failure $N" | tee -a $LOGS_FILE
+    exit 1
+else
+    echo -e "Installing $2 is $G successful $N" | tee -a $LOGS_FILE
+fi
+
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying mongo repo"
+
+dnf install mongodb-org -y 
+VALIDATE $? "Installing mongodb"
+
+systemctl enable mongod 
+VALIDATE $? "Enabling mongodb"
+
+systemctl start mongod 
+VALIDATE $? "Starting mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl restart mongod
+VALIDATE $? "Restarted mongodb"
